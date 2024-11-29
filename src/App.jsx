8de32d7c7dee5ea57c936e0ec804d56e40531893
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2024-11-29 12:02:58 +0530

    useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Counter from './components/state/Counter'
+import CounteruseState from './components/CounteruseState'
 
 function App() {
   return (
    <>
    
-   <Counter/>
+    <Counter/> 
+   <CounteruseState/>
    </>
   )
 }
--- src/components/CounteruseState.jsx
+++ src/components/CounteruseState.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 
 function Counter() {
   const [count,setCount]=useState(0);
@@ -11,13 +11,10 @@ function Counter() {
   const handlereset=()=>{
     setCount(0);
   }
-  useEffect(()=>{
-    console.log("component mounted");
-    
-  })
+ 
   return (
     <>
-    <h1>Counter-useEffect:{count}</h1>
+    <h1>Counter:{count}</h1>
     <button onClick={handleincrement}>+++</button>
     <button onClick={handledecrement}>---</button>
     <button onClick={handlereset}>000</button>
