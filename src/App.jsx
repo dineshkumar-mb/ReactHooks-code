69a5cca25b6384562ce251994fd45e0c2e6dba53
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2024-11-29 10:42:34 +0530

    React-hooks(useState)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './components/state/Counter'
+
+function App() {
+  return (
+   <>
+   
+   <Counter/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/state/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+function Counter() {
+  const[count,setCount]=useState(0);
+  const handleincrement=()=>{
+    setCount(count+1);
+  }
+  const handledecrement=()=>{
+    setCount(count-1);
+  }
+  const handlereset=()=>{
+    setCount(0);
+
+  }
+ 
+  return (
+    <>
+    <h1>Counter:{count}</h1>
+    <button onClick={handleincrement}>+++</button>
+    <button onClick={handledecrement}>---</button>
+    <button onClick={handlereset}>000</button>
+    </>
+  )
+}
+
+export default Counter
